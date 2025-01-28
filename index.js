61e5073757b67ed7651ea6544251ab0ce6d50358
mininxd <mininsna@gmail.com>
2025-01-28 11:04:34 +0700

    fix duplicate import, change useragent to android

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import fs from 'fs/promises';
 import chalk from 'chalk';
 import ora from 'ora';
 import Table from 'cli-table3';
-import { SocksProxyAgent } from 'socks-proxy-agent';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import figlet from 'figlet';
 
@@ -110,7 +109,7 @@ const getStats = async (token, proxyConfig = null) => {
   const headers = {
     'Accept': 'application/json',
     'Authorization': `Bearer ${token}`,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
     'Referer': 'https://depined.org/',
     'Origin': 'https://depined.org/',
     'Accept-Language': 'en-US,en;q=0.9',
@@ -145,7 +144,7 @@ const getUserProfile = async (token, proxyConfig = null) => {
   const headers = {
     'Accept': 'application/json',
     'Authorization': `Bearer ${token}`,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
     'Referer': 'https://depined.org/',
     'Origin': 'https://depined.org/',
     'Accept-Language': 'en-US,en;q=0.9',
@@ -180,7 +179,7 @@ const ping = async (token, proxyConfig = null) => {
   const headers = {
     'Accept': 'application/json',
     'Authorization': `Bearer ${token}`,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
     'Referer': 'https://depined.org/',
     'Origin': 'https://depined.org/',
     'Accept-Language': 'en-US,en;q=0.9',
